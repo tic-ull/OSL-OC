e1314710f73adb4711c6833c72b36d8666a9091b
alu0100819441 <alu0100819441@ull.edu.es>
2018-06-15 19:14:42 +0100

    Añade paginación a la tabla de software privativo

--- plugins/product-catalog/admin/alternative_to_view.php
+++ plugins/product-catalog/admin/alternative_to_view.php
@@ -21,84 +21,104 @@
     <?php 
         if ( empty( $_GET['id'] ) ) {
             $software_query = "SELECT * FROM " . $wpdb->get_blog_prefix();
-            $software_query .= "huge_it_catalog_proprietary_software ORDER BY name ASC";
-            $software_items = $wpdb->get_results( $software_query, ARRAY_A );
-    ?>
-    <?php 
-        if ( isset( $_GET['message'] ) ) {
-            switch($_GET['message']) {
-                case 0:
-                ?>
-                    <div id='message' class='updated fade'>
-                        <p><strong>Se debe proporcionar un nombre.</strong></p>
-                    </div>
-                <?php
-                break;
-                case 1:
-                ?>
-                    <div id='message' class='updated fade'>
-                        <p><strong>Software almacenado.</strong></p>
-                    </div>
-                <?php
-                break;
-                case 2:
-                ?>
-                    <div id='message' class='updated fade'>
-                        <p><strong>Software actualizado.</strong></p>
-                    </div>
-                <?php
-                break;
-                case 3:
-                ?>
-                    <div id='message' class='updated fade'>
-                        <p><strong>Software eliminado.</strong></p>
-                    </div>  
-                <?php
+            $software_query .= "huge_it_catalog_proprietary_software ORDER BY name ASC ";
+            $all_items = $wpdb->get_results( $software_query, ARRAY_A );
+            if ( isset( $_GET['page-number'] ) && !empty( $_GET['page-number'] ) ) {
+                $page_number = $_GET['page-number'];
+            } else {
+                $page_number = 1;
             }
-        }
-        ?>
-            <h3>Gestión de software privativo</h3>
-                <div style="width: 50%">
-                <form method="post"
-                    action="<?php echo add_query_arg ( array ( 'page' => 'huge-it-alternative-to',), 
-                                                               'admin.php' ); ?>">
-                    <input type="hidden" name="delete" value="delete_huge_it_software" /> 
-                    <table class="wp-list-table widefat fixed">
-                        <thead>
-                            <tr>
-                                <th style="width: 30%">ID</th>
-                                <th style="width: 40%">Nombre</th>
-                                <th style="width: 30%"></th>
-                            </tr>
-                        </thead>
-                        <?php
-                            if ( $software_items ) {
-                                foreach ( $software_items as $software_item ) {
-                                    echo '<tr style="background: #FFF">';
-                                        echo '<td>' . $software_item['id'] . '</td>';
-                                        echo '<td><a href="';
-                                        echo add_query_arg( array(
-                                                'page' => 'huge-it-alternative-to',
-                                                'id' => $software_item['id'] ),
-                                                'admin.php'
-                                            );
-                                        echo '">' . $software_item['name'] . '</a></td>';
-                                        echo '<td><input type="checkbox" name="items[]" value="';
-                                                echo intval( $software_item["id"] ) . '" /></td>';
-                                    echo '</tr>';
-                                }
-                            } else {
-                                echo '<tr style="background: #FFF">';
-                                echo '<td colspan="4">No existe software privativo en la base de datos.';
-                            }
+            $software_query .= "LIMIT " . ( $page_number - 1 ) * 20 . ", 20";
+            $software_items = $wpdb->get_results( $software_query, ARRAY_A );
+            
+            $number_of_pages = ceil(count($all_items) / 20);
+            
+    ?>
+            <?php 
+                if ( isset( $_GET['message'] ) ) {
+                    switch($_GET['message']) {
+                        case 0:
                         ?>
-                    </table>
+                            <div id='message' class='updated fade'>
+                                <p><strong>Se debe proporcionar un nombre.</strong></p>
+                            </div>
+                        <?php
+                        break;
+                        case 1:
+                        ?>
+                            <div id='message' class='updated fade'>
+                                <p><strong>Software almacenado.</strong></p>
+                            </div>
+                        <?php
+                        break;
+                        case 2:
+                        ?>
+                            <div id='message' class='updated fade'>
+                                <p><strong>Software actualizado.</strong></p>
+                            </div>
+                        <?php
+                        break;
+                        case 3:
+                        ?>
+                            <div id='message' class='updated fade'>
+                                <p><strong>Software eliminado.</strong></p>
+                            </div>  
+                        <?php
+                    }
+                }
+                ?>
+                <h3>Gestión de software privativo</h3>
+                    <div style="width: 50%">
+                        <div style="text-align: right; margin-right: 0.5%; font-size: medium">
+                            <?php
+                                for ($i = 1; $i <= $number_of_pages; $i++) {
+                                    echo "<a href=" . add_query_arg( array (
+                                            'page' => 'huge-it-alternative-to',
+                                            'page-number' => $i
+                                        ), 'admin.php' ) . " style='margin:0.3%'>" . $i . "</a>"; 
+                                } 
+                            ?>
+                        </div>
+                        <form method="post"
+                            action="<?php echo add_query_arg ( array ( 'page' => 'huge-it-alternative-to',), 
+                                                                    'admin.php' ); ?>">
+                            <input type="hidden" name="delete" value="delete_huge_it_software" /> 
+                            <table class="wp-list-table widefat fixed">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 30%">ID</th>
+                                        <th style="width: 40%">Nombre</th>
+                                        <th style="width: 30%"></th>
+                                    </tr>
+                                </thead>
+                                <?php
+                                    if ( $software_items ) {
+                                        foreach ( $software_items as $software_item ) {
+                                            echo '<tr style="background: #FFF">';
+                                                echo '<td>' . $software_item['id'] . '</td>';
+                                                echo '<td><a href="';
+                                                echo add_query_arg( array(
+                                                        'page' => 'huge-it-alternative-to',
+                                                        'id' => $software_item['id'] ),
+                                                        'admin.php'
+                                                    );
+                                                echo '">' . $software_item['name'] . '</a></td>';
+                                                echo '<td><input type="checkbox" name="items[]" value="';
+                                                        echo intval( $software_item["id"] ) . '" /></td>';
+                                            echo '</tr>';
+                                        }
+                                    } else {
+                                        echo '<tr style="background: #FFF">';
+                                        echo '<td colspan="4">No existe software privativo en la base de datos.';
+                                    }
+                                ?>
+                            </table>
+                            <br />
+                            <input style="float: right;" type="submit" value="Eliminar seleccionados" class="button-primary" />
+                        </form>
+                    </div>
                     <br />
-                    <input style="float: right;" type="submit" value="Eliminar seleccionados" class="button-primary" />
-                </form>
-                </div>
-                <br />
-        <?php
+            <?php
 
         } elseif ( isset( $_GET['id'] ) && ( 'new' == $_GET['id'] || is_numeric ( $_GET['id'] ) ) ) {
             $software_id = intval( $_GET['id'] );
